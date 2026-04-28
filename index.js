927d4663b18930b226a8bbb39a8704b811dc2255
riday-kumar <hridoykumarsaha07@gmail.com>
2026-04-29 02:27:57 +0600

    task update api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyFBToken = (req, res, next) => {
+  next();
+};
 
 app.get("/", (req, res) => {
   res.send("What will be your next target");
@@ -46,11 +50,44 @@ async function run() {
       res.send({ success: true });
     });
 
+    // all task getting api
+    app.get("/all-task", verifyFBToken, async (req, res) => {
+      const query = req.query.email;
+      // console.log(query);
+      const cursor = tasksCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // specific task shown
+    app.get("/all-task/:taskId", async (req, res) => {
+      const param = req.params.taskId;
+      const query = { _id: new ObjectId(param) };
+      const result = await tasksCollection.findOne(query);
+      res.send(result);
+    });
+
     // task create api
     app.post("/add-task", async (req, res) => {
       // console.log(req.body);
       const newTask = req.body;
       const result = await tasksCollection.insertOne(newTask);
+      res.send(result);
+    });
+
+    // task update
+    app.patch("/update-task/:taskId", async (req, res) => {
+      console.log(req.params.taskId);
+      const filter = { _id: new ObjectId(req.params.taskId) };
+      const updateDoc = {
+        $set: {
+          taskTitle: req.body.updatedTitle,
+          taskDescription: req.body.updatedDescription,
+          taskPriority: req.body.updatedPriority,
+          taskTime: req.body.updatedDate,
+        },
+      };
+      const result = await tasksCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
