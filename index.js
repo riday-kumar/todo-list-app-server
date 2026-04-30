5bb3e062630545174fb5c1ba19a995ef601a38c6
riday-kumar <hridoykumarsaha07@gmail.com>
2026-04-30 12:30:12 +0600

    config for deploy

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const port = process.env.PORT || 3000;
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./fbaccountkey.json");
+const decoded = Buffer.from(
+  process.env.FIREBASE_SERVICE_KEY,
+  "base64",
+).toString("utf8");
+const serviceAccount = JSON.parse(decoded);
+
 const { getAuth } = require("firebase-admin/auth");
 
 admin.initializeApp({
