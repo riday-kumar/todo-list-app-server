e61a4334fadc8ccc173d0084e5e2257247fad8f5
riday-kumar <hridoykumarsaha07@gmail.com>
2026-04-30 15:21:01 +0600

    remove condition

--- index.js
+++ index.js
@@ -86,9 +86,9 @@ async function run() {
     // all task getting api
     app.get("/all-task", async (req, res) => {
       const query = req.query.email;
-      if (query !== req.token_email) {
-        return res.status(403).send({ message: "Forbidden Access" });
-      }
+      // if (query !== req.token_email) {
+      //   return res.status(403).send({ message: "Forbidden Access" });
+      // }
       const filter = { userEmail: query };
       const cursor = tasksCollection.find(filter);
       const result = await cursor.toArray();
